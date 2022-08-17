f35024d6f19f61759b211311977ada7a266c9090
Hieser21 <96245505+Hieser21@users.noreply.github.com>
2022-08-17 17:12:33 +0600

    Update index.js

--- index.js
+++ index.js
@@ -58,7 +58,7 @@ smtpTrans.sendMail(mailOpts,(error,res)=>{
    }
    else{
     console.log("Message sent: " + res.message);
-    response.status(200).send("alert("Your email has been received!"); window.location.href = "https:/\/arafatrahaman.cyclic.app/"; ");
+    response.status(200).send('alert("Your email has been received!") window.location.href = "https:/\/arafatrahaman.cyclic.app/"');
     }
   //smtpTrans.close();
    })
